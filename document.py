b3667fc684366dd6eec1e77ae239c003ec991123
sascha <sascha.bub@xlandscape.org>
2023-09-11 23:14:39 +0200

    Updated Landscape Model core to version 1.15.1; Updated AnalysisObserver to version 2.2.4; Updated CascadeToxswa to version 2.2.3; Updated CmfContinuous to version 2.0.17; Updated LEffectModel to version 2.1.2; Updated LP50 to version 2.2.5; Updated ReportingObserver to version 2.0.13; Updated StepsRiverNetworkto version 2.1.4; Updated StreamCom to version 2.3.1; Updated XSprayDrift to version 2.5.2; Updated Oudebeek Beek 7 sample scenario to version 3.20; Added automatic documentation contributing notes, repository info and latest versions, and added variant checks; Added model metadata

--- document.py
+++ document.py
@@ -3,6 +3,18 @@ Script for documenting the Landscape Model xAquaticRisk variant.
 """
 import os
 import base.documentation
+import json
+import distutils.version
 
-root_folder = os.path.abspath(os.path.join(os.path.dirname(base.__file__), ".."))
-base.documentation.write_contribution_notes(os.path.join(root_folder, "..", "..", "CONTRIBUTING.md"))
+root_folder = os.path.abspath(os.path.join(os.path.dirname(base.__file__)))
+with open(os.path.join(root_folder, "..", "..", "..", "model.json")) as f:
+    version = distutils.version.StrictVersion(json.load(f)["version"])
+base.documentation.check_variant_parts(root_folder)
+base.documentation.write_contribution_notes(os.path.join(root_folder, "..", "..", "..", "CONTRIBUTING.md"))
+base.documentation.write_repository_info(
+    os.path.join(root_folder, "..", "..", ".."),
+    os.path.join(root_folder, "..", "..", "..", "repository.json"),
+    os.path.join(root_folder, "..", "..", "..", "..", "versions.json"),
+    "model"
+)
+base.documentation.write_latest_version_info(os.path.join(root_folder, "..", "..", "..", "latest_versions.json"))
